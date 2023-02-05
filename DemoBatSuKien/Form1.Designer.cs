3281fc2b5be88f263c3d60e1a17f36e35d138a2a
Pham Nguyet Thu <Pham Nguyet Thu@DESKTOP-P9S4ORN>
2023-02-05 20:13:24 +0700

    Add project files.

---
+++ DemoBatSuKien/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DemoBatSuKien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // user
+            // 
+            this.user.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user.Location = new System.Drawing.Point(308, 156);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(255, 29);
+            this.user.TabIndex = 0;
+            // 
+            // pass
+            // 
+            this.pass.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.Location = new System.Drawing.Point(308, 215);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(255, 29);
+            this.pass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(188, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(188, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // Login
+            // 
+            this.Login.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.Location = new System.Drawing.Point(340, 286);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(114, 38);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Đăng nhập";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Login;
+    }
+}
+
---
+++ DemoBatSuKien/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoBatSuKien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            //Bắt sự kiện textbox Username và Password bỏ trống.
+            //if(user.Text.Length == 0 && pass.Text.Length == 0)
+            //{
+            //    MessageBox.Show("Username và password không được bỏ trống");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Thành công!");
+            //}
+
+            //Bắt sự kiện username và password không đúng
+            if(user.Text == "admin" && pass.Text =="123")
+            {
+                MessageBox.Show("Tài khoản đúng");
+            } else
+            {
+                MessageBox.Show("Tài khoản không đúng");
+            }
+        }
+    }
+}
